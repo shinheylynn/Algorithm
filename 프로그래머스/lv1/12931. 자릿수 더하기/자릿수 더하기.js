f68382f670042aa1d68be660de2ab78b415aee34
Heylynn Shin <heylub@gmail.com>
2023-04-03 17:16:31 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+ let arr = [];
+    do {
+        arr.push(n % 10);
+        n = Math.floor(n / 10);
+    } while (n > 0);
+    return arr.reduce((idx, acc) => idx + acc, 0)
+}
