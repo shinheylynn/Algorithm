1cdb877bcec5e52e25bb00ca3150eb11e170ad56
Heylynn Shin <heylub@gmail.com>
2023-04-10 17:34:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,13 @@
+function solution(num) {
+    for (i=0; i<=500; i++) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else if (num == 1) {
+            return i;
+        } else {
+            num = (num * 3) + 1;
+        }
+    }
+    
+    return -1;
+}
