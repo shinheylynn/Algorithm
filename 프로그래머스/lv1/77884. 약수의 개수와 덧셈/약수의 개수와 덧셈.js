52a1d8c7a10388eba3bde1469e0af39e16bf08de
Heylynn Shin <heylub@gmail.com>
2023-04-26 17:52:37 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,13 @@
+function solution(left, right) {
+    let answer = 0;
+    
+    for (i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    
+    return answer;
+}
