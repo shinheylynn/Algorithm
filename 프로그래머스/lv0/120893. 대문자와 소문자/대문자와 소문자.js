1cd488a6770d6bab8ea48d3f565dc58aa930657c
Heylynn Shin <heylub@gmail.com>
2023-05-12 15:01:38 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let answer = "";
+    
+    for (let i of my_string) {
+        if (i === i.toUpperCase()) {
+            answer += i.toLowerCase()
+        } else answer += i.toUpperCase()
+    }
+    return answer;
+}
