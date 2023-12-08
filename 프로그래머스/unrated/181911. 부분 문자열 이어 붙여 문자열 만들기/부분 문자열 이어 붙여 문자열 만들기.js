f59cd7b7f30aaf3a1395f79b5357ec7db7d9dd72
Heylynn Shin <heylub@gmail.com>
2023-12-08 14:26:14 +0900

    [level unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,9 @@
+function solution(my_strings, parts) {
+    let answer = [];
+    
+    for (i = 0; i < my_strings.length; i++) {
+        answer.push(my_strings[i].slice(parts[i][0], parts[i][1] + 1))
+    }
+    
+    return answer.join("")
+}
