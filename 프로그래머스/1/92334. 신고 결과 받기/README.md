77b2454d7d98d9f2d76aadb632f032cbe5b83158
Heylynn Shin <heylub@gmail.com>
2025-02-28 16:52:54 +0900

    [level 1] Title: 신고 결과 받기, Time: 256.79 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 28일 02:55:42
+2025년 02월 28일 16:52:51
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,6 +1,30 @@
 function solution(id_list, report, k) {
-    for (let i = 0; i < report.length; i++) {
-        return [reporter, blocked] = report[i].split(" ")
-    }
-    console.log([reporter, blocked])
+    const reports = new Map()
+    const reportedCount = new Map()
+    
+    id_list.forEach(user => {
+        reports.set(user, new Set())
+        reportedCount.set(user, 0)
+    })
+    
+    report.forEach(entry => {
+        const [reporter, reported] = entry.split(' ')
+        
+        if (!reports.get(reporter).has(reported)) {
+            reports.get(reporter).add(reported)
+            reportedCount.set(reported, reportedCount.get(reported) + 1)
+        }
+    })
+    
+    const banned = new Set()
+    reportedCount.forEach((count, user) => {
+        if (count >= k) {
+            banned.add(user)
+        }
+    })
+    
+    
+    return id_list.map(reporter => 
+        [...reports.get(reporter)].filter(reported => banned.has(reported)).length
+    )
 }
