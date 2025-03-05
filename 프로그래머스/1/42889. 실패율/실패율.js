bcc29469782b5aaf759ed3084a896888e019211c
Heylynn Shin <heylub@gmail.com>
2025-03-05 14:39:06 +0900

    [level 1] Title: 실패율, Time: 1808.34 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,10 +1,22 @@
 function solution(N, stages) {
-    let result = [];
-    for(let i=1; i<=N; i++){
-        let reach = stages.filter((x) => x >= i).length;
-        let curr = stages.filter((x) => x === i).length;
-        result.push([i, curr/reach]);
+    const failureRateMap = new Map()
+    
+    for (let i = 1; i <= N; i++) {
+        let reached = 0
+        let failed = 0
+        
+        stages.forEach(stage => {
+            if (stage >= i) {
+                reached++
+            } 
+            if (stage === i) {
+                failed++
+            }
+        })
+        
+        const failureRate = failed / reached
+        failureRateMap.set(i, failureRate)
     }
-    result.sort((a,b) => b[1] - a[1]);
-    return result.map((x) => x[0]);
+
+    return [...failureRateMap].sort((a, b) => b[1] - a[1]).map(item => item[0])
 }
