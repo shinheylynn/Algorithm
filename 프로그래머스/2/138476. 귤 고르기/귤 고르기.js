7da2dba8a2f1f6cf675739f870dbc75a5b1a69a1
Heylynn Shin <heylub@gmail.com>
2025-04-30 00:22:32 +0900

    [level 2] Title: 귤 고르기, Time: 25.10 ms, Memory: 47.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,24 @@
+function solution(k, tangerine) {
+    const map = new Map()
+    let answer = 0
+    
+    tangerine.forEach(i => {
+        map.set(i, (map.get(i) || 0) + 1)
+    })
+    
+    const sortedSize = Array.from(map.values()).sort((a, b) => b - a)
+    
+    for (let i = 0; i < sortedSize.length; i++) {
+        if (sortedSize[i] >= k && i === 0) {
+            answer = 1
+            break
+        } 
+        
+        if (k > 0) {
+            k = k - sortedSize[i]
+            answer++
+        } 
+    }
+    
+    return answer
+}
