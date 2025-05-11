4c55dca4888a7bc4a3e3eaacb1262e38f6663f4d
Heylynn Shin <heylub@gmail.com>
2025-05-12 02:37:46 +0900

    [level 2] Title: 주차 요금 계산, Time: 8.98 ms, Memory: 35.2 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -6,31 +6,32 @@ function getTimeDiff(t1, t2) {
 }
 
 function getTotalFee(time, [baseTime, baseFee, unitTime, unitFee]) {
-  if (time <= baseTime) return baseFee;
-  return baseFee + Math.ceil((time - baseTime) / unitTime) * unitFee;
+  if (time <= baseTime) return baseFee
+  return baseFee + Math.ceil((time - baseTime) / unitTime) * unitFee
 }
 
 function solution(fees, records) {
-    const arr = records
-    .map(e => e.split(' '))
-    .sort((a, b) => Number(a[1]) - Number(b[1]))
-    
     if (records.length === 1) {
-        return [getTotalFee(getTimeDiff(arr[0][0], '23:59'), fees)]
-    }
+        const [time, car] = records[0].split(' ')
+        const totalTime = getTimeDiff(time, '23:59')
+        return [getTotalFee(totalTime, fees)]
+      }
     
-    const map = new Map()
+    const obj = {}
     const answer = []
     
-    arr.forEach(e => {
-      const [time, car, action] = e
-      
-      if (!map.has(car)) map.set(car, [])
-      map.get(car).push(time)
-    })
+    for (const r of records) {
+        const [time, car] = r.split(' ')
+        
+        if(!obj[car]) obj[car] = []
+        obj[car].push(time)
+    }
     
-    for (const [car, times] of map) {
-        let totalDiff = times.reduce((acc, cur, i) => {
+    const sortedObj = Object.entries(obj)
+    .sort((a, b) => Number(a[0]) - Number(b[0]))
+    
+    for (const [car, times] of sortedObj) {
+        let totalDiff = times.reduce((acc, _, i) => {
             if (i % 2 === 0 && times[i + 1]) {
                 return acc + getTimeDiff(times[i], times[i + 1])
             }
