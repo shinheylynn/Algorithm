ea34390eb2023c6c5fa171a4d36a7cc5403978cd
Heylynn Shin <heylub@gmail.com>
2025-05-29 00:58:50 +0900

    [level 2] Title: 튜플, Time: 32.87 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -3,9 +3,9 @@ function solution(s) {
     const str = s.replace(/{/g, "[").replace(/}/g, "]")
     const arr = JSON.parse(str)
     
-    arr.sort((a, b) => a.length - b.length)
-
-    arr.map(item => {
+    arr
+    .sort((a, b) => a.length - b.length)
+    .map(item => {
         const num = item.filter(v => !answer.includes(v))
         answer.push(Number(num))
     })
