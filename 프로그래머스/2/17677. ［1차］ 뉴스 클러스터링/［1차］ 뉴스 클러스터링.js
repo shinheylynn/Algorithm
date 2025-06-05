cb90a089aecd9517e0b1cd68963f527b6178abaf
Heylynn Shin <heylub@gmail.com>
2025-06-05 14:32:38 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -3,8 +3,7 @@ const solution = (str1, str2) => {
     const arr1 = makeSet(str1.toLowerCase())
     const arr2 = makeSet(str2.toLowerCase())
     
-    const intersection = makeIntersection(arr1, arr2)
-    const union = makeUnion(arr1, arr2)
+    const [intersection, union] = makeUnionAndIntersection(arr1, arr2)
     
     if (intersection === 0 && union === 0) {
         jaccard = 1
@@ -21,31 +20,13 @@ const makeSet = (str) => {
             arr.push(x + str[idx + 1])
         }
     })
-    
-    return arr.filter(x => /^[a-z|A-Z]+$/.test(x))
+
+    return arr.filter(x => /[a-zA-Z]{2}/.test(x))
 }
 
-const makeIntersection = (arr1, arr2) => {
-    const map = new Map()
-    const result = []
-    
-    for (const char of arr1) {
-        map.set(char, (map.get(char) || 0) + 1)
-    }
-    
-    for (const char of arr2) {
-        const count = map.get(char) || 0
-        if (count > 0) {
-            result.push(char)
-            map.set(char, map.get(char) - 1)
-        }
-    }
-    
-    return result.length
-}
-
-const makeUnion = (arr1, arr2) => {
-    const result = []
+const makeUnionAndIntersection = (arr1, arr2) => {
+    let union = 0
+    let intersection = 0
     const count1 = new Map()
     const count2 = new Map()
     
@@ -59,9 +40,12 @@ const makeUnion = (arr1, arr2) => {
     
     const unionKeys = new Set([...count1.keys(), ...count2.keys()])
     for (const char of unionKeys) {
+        const minCount = Math.min(count1.get(char) || 0, count2.get(char) || 0)
         const maxCount = Math.max(count1.get(char) || 0, count2.get(char) || 0)
-        result.push(...Array(maxCount).fill(char))
+        
+        if (minCount > 0) intersection += minCount
+        union += maxCount
     }
     
-    return result.length
+    return [intersection, union]
 }
