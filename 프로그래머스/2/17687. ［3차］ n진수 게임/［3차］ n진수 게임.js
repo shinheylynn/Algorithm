9b85ec5f0f7b654b37ebbe6fbf72447c6c1bf79e
Heylynn Shin <heylub@gmail.com>
2025-07-02 01:22:31 +0900

    [level 2] Title: [3차] n진수 게임, Time: 163.16 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,13 +1,13 @@
 function solution(n, t, m, p) {
-    let answer = []
+    let answer = ''
     const arrN = Array(t * m).fill(0).map((_, i) => i.toString(n).toUpperCase())
     const nums = arrN.map(n => n.split('')).flat()
 
     nums.forEach((n, idx) => {
         if (idx % m === (p - 1)) {
-            answer.push(n)
+            answer += n
         }
     })
     
-    return answer.splice(0, t).join('')
+    return answer.slice(0, t)
 }
