e740bf0a395ddf23f31239ab612efe24e353cae8
Heylynn Shin <heylub@gmail.com>
2025-07-09 10:22:14 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 34.10 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,7 +1,6 @@
 const solution = (n, k) => {
     let answer = 0
-    const converted = n.toString(k)
-    const nums = converted.split('0')
+    const nums = n.toString(k).split('0')
     
     for (let num of nums) {
         if (num === '') continue
