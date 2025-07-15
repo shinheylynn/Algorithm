84aaf9381ab56b49aef612e6ec3aeabc88d3d7d7
Heylynn Shin <heylub@gmail.com>
2025-07-15 23:47:29 +0900

    [level 2] Title: [3차] 압축, Time: 1847.12 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,22 +1,25 @@
 function solution(msg) {
     const dic = new Map('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((w, i) => [w, i + 1]))
     const answer = []
-    let arr = [...msg]
-    let w = ''
-    let n = 0
-    
-   while (arr.length > 0) {
-       if (dic.get(arr.join(''))) {
-           n += arr.join('').length
-           answer.push(dic.get(arr.join('')))
-           dic.set(arr.join('') + w, dic.size + 1)
-           arr = [...msg]
-           arr.splice(0, n)
-        } else {
-            w = arr[arr.length - 1]
-            arr.pop()
+
+    let sliceIndex = 0
+    let c = ''
+
+    while (sliceIndex < msg.length) {
+        let arr = msg.slice(sliceIndex).split('')
+        let w = arr.join('')
+
+        // w가 사전에 없다면, 마지막 글자 하나씩 제거하며 찾는다
+        while (!dic.get(w)) {
+            c = arr.pop()
+            w = arr.join('')
         }
-   }
-    
+
+        answer.push(dic.get(w))
+        dic.set(w + c, dic.size + 1)
+
+        sliceIndex += w.length
+    }
+
     return answer
 }
