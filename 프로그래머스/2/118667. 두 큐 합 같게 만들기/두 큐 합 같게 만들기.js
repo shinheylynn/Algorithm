d0fb2182a44edac7faca72adc86600f033be49fc
Heylynn Shin <heylub@gmail.com>
2025-07-22 23:40:17 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 60.37 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,22 @@
+function solution(queue1, queue2) {
+    const queue = [...queue1, ...queue2]
+    const half = queue.reduce((acc, cur) => acc + cur, 0) / 2
+    
+    let sum1 = queue1.reduce((acc, cur) => acc + cur, 0)
+    let index1 = 0
+    let index2 = queue1.length
+    let answer = 0
+    while (answer <= (queue1.length + queue2.length) * 2) {
+        if (sum1 === half) return answer
+        
+        if (sum1 > half) {
+            sum1 -= queue[index1++]
+        } else {
+            sum1 += queue[index2++]
+        }
+        
+        answer++
+    }
+    
+    return -1
+}
