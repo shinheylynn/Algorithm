d6910096d17565f4aed222f7a63f4dc3c27c8bbd
Heylynn Shin <heylub@gmail.com>
2025-08-19 21:47:17 +0900

    Time: 0 ms (100%), Space: 56.8 MB (98.7%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -15,24 +15,8 @@
 function maxDepth(root: TreeNode | null): number {
       if (root === null) return 0
 
-      const queue = []
-      queue.push(root)
-      let depth = 0
+      const leftDepth = maxDepth(root!.left)
+      const rightDepth = maxDepth(root!.right)
 
-      while (queue.length > 0) {
-        depth++
-
-        const size = queue.length
-        for (let i = 0; i < size; i++) {
-            const node = queue.shift()
-            if (node.left) {
-                queue.push(node.left)
-            }
-            if (node.right) {
-                queue.push(node.right)
-            }
-        }
-      }
-
-      return depth
+      return 1 + Math.max(leftDepth, rightDepth)
 };
