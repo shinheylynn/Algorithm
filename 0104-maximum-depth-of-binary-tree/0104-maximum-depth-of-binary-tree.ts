b0714a61b29f36728a04d0f9d90a633e4bbecc53
Heylynn Shin <heylub@gmail.com>
2025-08-19 21:23:07 +0900

    Time: 2 ms (22.45%), Space: 59.8 MB (15.45%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+      if (root === null) return 0
+
+      const queue = []
+      queue.push(root)
+      let depth = 0
+
+      while (queue.length > 0) {
+        depth++
+
+        const size = queue.length
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift()
+            if (node.left) {
+                queue.push(node.left)
+            }
+            if (node.right) {
+                queue.push(node.right)
+            }
+        }
+      }
+
+      return depth
+};
