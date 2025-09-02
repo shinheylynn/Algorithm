8bbdb80be30b1bb8760e2a77383697a0b393109c
Heylynn Shin <heylub@gmail.com>
2025-09-02 22:00:42 +0900

    Time: 0 ms (100%), Space: 53.7 MB (98.22%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.ts
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -1,6 +1,18 @@
 function searchInsert(nums: number[], target: number): number {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target || nums[i] > target) return i
+    let left = 0
+    let right = nums.length - 1
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
     }
-    return nums.length
-};
+
+    return left 
+}
