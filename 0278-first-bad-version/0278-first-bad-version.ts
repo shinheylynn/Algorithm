b3c3586448f3fe80aa421686fcf526a7c91ada66
Heylynn Shin <heylub@gmail.com>
2025-09-09 21:10:48 +0900

    Time: 39 ms (80.09%), Space: 53.3 MB (95.77%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.ts
+++ 0278-first-bad-version/0278-first-bad-version.ts
@@ -1,23 +1,12 @@
-/**
- * The knows API is defined in the parent class Relation.
- * isBadVersion(version: number): boolean {
- *     ...
- * };
- */
-
 var solution = function(isBadVersion: any) {
-    return function(n: number): number {
-        let min = 0
-        let max = n
+  const func = function(n: number, min = 1, max = n): number {
+    if (min === max) return min
 
-        while (min < max) {
-            const mid = Math.floor((min + max) / 2)
-            if (isBadVersion(mid)) {
-                max = mid
-            } else {
-                min = mid + 1
-            }
-        }
-        return max
-    };
-};
+    const mid = Math.floor((min + max) / 2)
+    if (isBadVersion(mid)) {
+        return func(n, min, mid)
+    }
+    return func(n, mid + 1, max)
+  }
+  return func
+}
