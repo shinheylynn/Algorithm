230c6387f39f5ff4e8a31d3675074485398dc069
Heylynn Shin <heylub@gmail.com>
2025-10-22 15:46:05 +0900

    Time: 0 ms (100%), Space: 55.7 MB (76.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -1,7 +1,7 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   let p1 = m - 1
   let p2 = n - 1
-  let p = m + n - 1
+  let p = nums1.length - 1
 
   while (p2 >= 0) {
     if (p1 >= 0 && nums1[p1] > nums2[p2]) {
