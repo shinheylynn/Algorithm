ec498dc748ca8bce64f000373f72aed4cade4222
Heylynn Shin <heylub@gmail.com>
2025-10-29 16:29:12 +0900

    Time: 3 ms (65.47%), Space: 59.8 MB (36.78%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.ts
@@ -0,0 +1,12 @@
+function majorityElement(nums: number[]): number {
+    const count = {}
+    const half = Math.floor(nums.length / 2)
+
+    for (const num of nums) {
+        count[num] = (count[num] || 0) + 1
+
+        if (count[num] > half) {
+            return num
+        }
+    }
+};
